21582cd4a3271d2b92259230b9403a35c3aaf302
Eduardo Souza <eduardo.barbosa@mprj.mp.br>
2025-11-11 17:36:40 -0300

    print sett

--- 02-encontros-tech/core/database.py
+++ 02-encontros-tech/core/database.py
@@ -3,6 +3,9 @@ from sqlalchemy.orm import sessionmaker
 from contextlib import contextmanager
 from core.settings import settings
 
+print(f"--- [DEBUG] Valor de settings.DATABASE_URL: {settings.DATABASE_URL} ---")
+print(f"--- [DEBUG] Tipo de settings.DATABASE_URL: {type(settings.DATABASE_URL)} ---")
+
 engine = create_engine(settings.DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
